078d6f287c5c16087b2a087324b9da1c9ca6d835
antirez <antirez@gmail.com>
2012-02-19 17:28:45 +0100

    Fixed comment typos replacing map with array where appropriate.

--- lua_msgpack.c
+++ lua_msgpack.c
@@ -272,15 +272,15 @@ static void mp_encode_map(mp_buf *buf, int64_t n) {
     int enclen;
 
     if (n <= 15) {
-        b[0] = 0x80 | (n & 0xf);    /* fix array */
+        b[0] = 0x80 | (n & 0xf);    /* fix map */
         enclen = 1;
     } else if (n <= 65535) {
-        b[0] = 0xde;                /* array 16 */
+        b[0] = 0xde;                /* map 16 */
         b[1] = (n & 0xff00) >> 8;
         b[2] = n & 0xff;
         enclen = 3;
     } else {
-        b[0] = 0xdf;                /* array 32 */
+        b[0] = 0xdf;                /* map 32 */
         b[1] = (n & 0xff000000) >> 24;
         b[2] = (n & 0xff0000) >> 16;
         b[3] = (n & 0xff00) >> 8;
