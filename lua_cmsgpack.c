4ca832bf153154bac85328791c8888a1777b88ee
Matt Stancliff <matt@genges.com>
2014-04-06 21:27:45 -0400

    Malloc one buffer per pack() instead of per arg
    We can reuse the same malloc'd buffer for encoding multiple
    arguments.  Just tell the buffer all current contents is now
    free space and reset the current offset back to zero.
    
    We now only malloc once per pack() call instead of argc times
    per call, giving us better performance with large argument
    counts.
    
    All 226 tests pass.

--- lua_cmsgpack.c
+++ lua_cmsgpack.c
@@ -488,24 +488,28 @@ static void mp_encode_lua_type(lua_State *L, mp_buf *buf, int level) {
 static int mp_pack(lua_State *L) {
     int nargs = lua_gettop(L);
     int i;
+    mp_buf *buf;
 
     if (nargs == 0)
         return luaL_argerror(L, 0, "MessagePack pack needs input.");
 
-    /* Create nargs packed buffers */
+    buf = mp_buf_new();
     for(i = 1; i <= nargs; i++) {
-        mp_buf *buf;
-
         /* Copy argument i to top of stack for _encode processing;
          * the encode function pops it from the stack when complete. */
         lua_pushvalue(L, i);
 
-        buf = mp_buf_new();
         mp_encode_lua_type(L,buf,0);
 
         lua_pushlstring(L,(char*)buf->b,buf->len);
-        mp_buf_free(buf);
+
+        /* Reuse the buffer for the next operation by
+         * setting its free count to the total buffer size
+         * and the current position to zero. */
+        buf->free += buf->len;
+        buf->len = 0;
     }
+    mp_buf_free(buf);
 
     /* Concatenate all nargs buffers together */
     lua_concat(L, nargs);
