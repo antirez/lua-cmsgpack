587922d2957e1c5786128dd0da41bee05990494a
Matt Stancliff <matt@genges.com>
2014-11-24 11:52:32 -0500

    Update CI to test against 5.3.0 beta
    5.3.0-work2 has been obsoleted by the newer 5.3.0-beta
    
    This testing infrastructure is taken from:
    https://github.com/moteus/lua-vararg/tree/246dc3126dd66935a020a0a26c165ceb035268e5/.travis

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -1,13 +1,54 @@
+#! /bin/bash
+
 # A script for setting up environment for travis-ci testing.
 # Sets up Lua and Luarocks.
 # LUA must be "lua5.1", "lua5.2" or "luajit".
-# PLATFORM must be "linux" or "macosx".
+# luajit2.0 - master v2.0
+# luajit2.1 - master v2.1
 
-if [ "$LUA" == "luajit" ]; then
-  curl http://luajit.org/download/LuaJIT-2.0.2.tar.gz | tar xz
-  cd LuaJIT-2.0.2
+LUAJIT_BASE="LuaJIT-2.0.3"
+
+source .travis/platform.sh
+
+LUAJIT="no"
+
+if [ "$PLATFORM" == "macosx" ]; then
+  if [ "$LUA" == "luajit" ]; then
+    LUAJIT="yes";
+  fi
+  if [ "$LUA" == "luajit2.0" ]; then
+    LUAJIT="yes";
+  fi
+  if [ "$LUA" == "luajit2.1" ]; then
+    LUAJIT="yes";
+  fi;
+elif [ "$(expr substr $LUA 1 6)" == "luajit" ]; then
+  LUAJIT="yes";
+fi
+
+if [ "$LUAJIT" == "yes" ]; then
+
+  if [ "$LUA" == "luajit" ]; then
+    curl http://luajit.org/download/$LUAJIT_BASE.tar.gz | tar xz;
+  else
+    git clone http://luajit.org/git/luajit-2.0.git $LUAJIT_BASE;
+  fi
+
+  cd $LUAJIT_BASE
+
+  if [ "$LUA" == "luajit2.1" ]; then
+    git checkout v2.1;
+  fi
+
   make && sudo make install
-  cd $TRAVIS_BUILD_DIR;
+
+  if [ "$LUA" == "luajit2.1" ]; then
+    sudo ln -s /usr/local/bin/luajit-2.1.0-alpha /usr/local/bin/luajit
+    sudo ln -s /usr/local/bin/luajit /usr/local/bin/lua;
+  else
+    sudo ln -s /usr/local/bin/luajit /usr/local/bin/lua;
+  fi;
+
 else
   if [ "$LUA" == "lua5.1" ]; then
     curl http://www.lua.org/ftp/lua-5.1.5.tar.gz | tar xz
@@ -16,35 +57,45 @@ else
     curl http://www.lua.org/ftp/lua-5.2.3.tar.gz | tar xz
     cd lua-5.2.3;
   elif [ "$LUA" == "lua5.3" ]; then
-    curl http://www.lua.org/work/lua-5.3.0-work2.tar.gz | tar xz
-    cd lua-5.3.0-work2;
+    curl http://www.lua.org/work/lua-5.3.0-beta.tar.gz | tar xz
+    cd lua-5.3.0-beta;
   fi
-  sudo make $PLATFORM install
-  cd $TRAVIS_BUILD_DIR;
+  sudo make $PLATFORM install;
 fi
 
-LUAROCKS_BASE=luarocks-$LUAROCKS_VER
-curl http://luarocks.org/releases/$LUAROCKS_BASE.tar.gz | tar xz
-cd $LUAROCKS_BASE;
+cd $TRAVIS_BUILD_DIR;
+
+LUAROCKS_BASE=luarocks-$LUAROCKS
+
+# curl http://luarocks.org/releases/$LUAROCKS_BASE.tar.gz | tar xz
+
+git clone https://github.com/keplerproject/luarocks.git $LUAROCKS_BASE
+cd $LUAROCKS_BASE
+
+git checkout v$LUAROCKS
 
 if [ "$LUA" == "luajit" ]; then
   ./configure --lua-suffix=jit --with-lua-include=/usr/local/include/luajit-2.0;
+elif [ "$LUA" == "luajit2.0" ]; then
+  ./configure --lua-suffix=jit --with-lua-include=/usr/local/include/luajit-2.0;
+elif [ "$LUA" == "luajit2.1" ]; then
+  ./configure --lua-suffix=jit --with-lua-include=/usr/local/include/luajit-2.1;
 else
   ./configure;
 fi
 
-make && sudo make install
+make build && sudo make install
 
 cd $TRAVIS_BUILD_DIR
 
 rm -rf $LUAROCKS_BASE
 
-if [ "$LUA" == "luajit" ]; then
-  rm -rf LuaJIT-2.0.2;
+if [ "$LUAJIT" == "yes" ]; then
+  rm -rf $LUAJIT_BASE;
 elif [ "$LUA" == "lua5.1" ]; then
   rm -rf lua-5.1.5;
 elif [ "$LUA" == "lua5.2" ]; then
   rm -rf lua-5.2.3;
 elif [ "$LUA" == "lua5.3" ]; then
-  rm -rf lua-5.3.0-work2;
+  rm -rf lua-5.3.0-beta;
 fi
